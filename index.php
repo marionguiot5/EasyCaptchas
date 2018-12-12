ec1fc69ee9cf97db83b52aed2b8f199da6cdd180
marionguiot5 <45821971+marionguiot5@users.noreply.github.com>
2018-12-12 15:32:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <?php
 
 /* SCRIPT FRONTAL AFFICHAGE DE CAPTCHA
-* AFFICHER UNE IMAGE GÉNÉRÉE EN PHP */
+* AFFICHER UNE IMAGE GÃ‰NÃ‰RÃ‰E EN PHP */
+header( 'content-type: text/html; charset=utf-8' );
+echo "Veuillez saisir le code de sÃ©curitÃ©";
 
 echo "<img src='script-captchas.php' alt='captchas' />";
 
+if(isset($_REQUEST['envoyer']))
+
 ?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset='utf-8'>
+</head>  
+<body>
+<form action="script-captchas.php" method=POST>
+			<input type-required="text" name="captcha">
+			<input type="submit" id="Envoyer">
+</form>
+</body>
+</html>
